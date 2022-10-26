67a8431fed07f969c3b27ffd2c1bcb65d6f327c1
“erikayanez“ <erikajoam@gmail.com>
2022-10-26 11:17:26 +0200

    Client folder created with React app.  Navbar in progress

---
+++ client/nea-hotel/src/Pages/Stay/stay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Stay() {
+    return (
+        <div>
+            <h1>Rooms</h1>
+            <img src="https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjBjb2xvciUyMGZyYW1ld29yayUyMGNvbG9yc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="hotel" />
+        </div>
+    );
+}
+
