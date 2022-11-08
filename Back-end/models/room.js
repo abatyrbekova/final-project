0a2ac8db5ad92355be22d0e64235447a07065cf8
“erikayanez“ <erikajoam@gmail.com>
2022-11-08 10:28:19 +0100

    Json database for rooms and new pictures were added

--- Back-end/models/room.js
+++ Back-end/models/room.js
@@ -4,9 +4,9 @@ const { Schema, model } = mongoose;
 
 const roomSchema = new Schema({
     name: {type: String, required: true},
-    size: {type: Number, required: true},
+    size: {type: String, required: true},
     description: {type: String, required: true},
-    sleeps: {type: Number, required: true, min: 1, max: 4},
+    sleeps: {type: String, required: true, min: 1, max: 4},
     bedSize:{type:String, enum:['QUEEN', 'KING', 'SINGLE', 'DOUBLE']},
     view: {type: String, enum: ['MOUNTAIN', 'FOREST']},
     rating: {type: Number, min: 1, max: 5, default: 1},
