1e1e73c4e9278660e1584b8e7596ad40754b91e1
“erikayanez“ <erikajoam@gmail.com>
2022-11-10 11:52:51 +0100

    userRoutes was changed.  Login must be checked

--- Back-end/routes/userRoutes.js
+++ Back-end/routes/userRoutes.js
@@ -1,67 +1,26 @@
 import express from 'express';
-import User from '../models/user.js';
+import { getAllUsers, login, registerUser } from '../controllers/userController.js';
+// because of the controllers now we don't need: import User from '../models/user.js';
 
 const router = express.Router();
 
-//GET all users
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-});
+//GET::
+//http://localhost:3000/api/users/
+router.get('/', getAllUsers)
 
+//Post or Get? Login
+router.post('/login', login)
 
-//POST create a new user
-router.post('/create', async (req, res) => {
-    try {
-        const newUser = await User.create(req.body);
-        return res.status(201).json(newUser);
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
-});
+//POST::
+//http://localhost:3000/api/users/create
+//BODY
+// {
+// 	"firstName":"Brandon",
+// 	"lastName":"Test",
+// 	"userName":"Brandon123",
+// 	"email":"brandon.test@test.com"
+// }
+router.post('/register', registerUser);
 
-//User login
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "User not found" });
-    
-    const validPassword = await user.validatePassword(password);
-    if (!validPassword)
-        return res.status(401).json({ token: null, message: "Invalid password" });
-    
-    const token = jwt.sign({ id: user._id }, config.SECRET, {
-        expiresIn: 60 * 60 * 24,
-    });
-    
-    res.json({ token });
-});
-
-//User profile
-router.get("/profile", async (req, res) => {
-    const user = await User.findById(req.userId, { password: 0 });
-    if (!user) return res.status(404).json({ message: "No user found" });
-    
-    res.json(user);
-});
-
-//User Register
-router.post("/register", async (req, res) => {
-    const { email, password } = req.body;
-    const newUser = new User({ email, password });
-    await newUser.save();
-    
-    const token = jwt.sign({ id: newUser._id }, config.SECRET, {
-        expiresIn: 60 * 60 * 24,
-    });
-    
-    res.status(201).json({ token });
-});
 
 export default router;
-
