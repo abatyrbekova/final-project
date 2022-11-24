edca40dc5a96aa9eb2718ebf7c6e6bb95a6e74ec
abatyrbekova <batyrbekova592@gmail.com>
2022-11-24 21:40:07 +0100

    navbar styling

--- client/nea-hotel/src/App.js
+++ client/nea-hotel/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 
-import Navbar from "./Components/navbar";
+import Navbar from "./Components/navbar/navbar";
 import Home from "./Pages/Home/home";
 import Stay from "./Pages/Stay/stay";
 import Activities from "./Pages/Activities/activities";
---
+++ client/nea-hotel/src/Components/navbar/navbar.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./navbar.css";
+
+import NavbarLogo from "./img/nea_hotel_logo.jpg";
+// import NavbarLogo from "./img/nea_hotel_light_logo.png";
+// import NavbarLogo from "./img/nea_hotel_dark_logo.png";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  const [isTransparent, setIsTransparent] = useState(true);
+
+  const changeColor = () => {
+    console.log("changeColor");
+    if (window.scrollY >= 90) {
+      setIsTransparent(false);
+    } else {
+      setIsTransparent(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
+  return (
+    <nav className={isTransparent ? "nav" : "nav nav-scrolled"}>
+      <div className="navbar-logo">
+        <NavLink to="/">
+          <img className="logo" src={NavbarLogo} />
+        </NavLink>
+      </div>
+      <ul className="navbar-links">
+        <li className="nav-link">
+          <NavLink to="/stay">Accommodation</NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/menu">Eat & Drink</NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/activities">Activities</NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+      <div className="navbar__auth">
+        <button
+          className="btn btn--primary"
+          onClick={() => navigate("/booking")}
+        >
+          Book Now
+        </button>
+      </div>
+    </nav>
+  );
+}
